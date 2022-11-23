3e7918c838387904a2babe6c4a1ecc731a70ff74
Amina Hamna <aminahamnapt78@gmail.com>
2022-11-24 01:29:31 +0530

    1st commit

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+    return (
+        <div 
+         className='banner'>
+            <div className='content' >
+                <h1 className='title'>Movie Name  </h1>
+                <div className='banner_buttons' >
+                    <button className='button' >Play</button>
+                    <button className='button' >My playlist</button>
+                </div>
+                <h1 className='description'>The movie directed by hffjd  and kikj nhvjgun kakak ftbki vbghyt mmcdhfj adsfhg bhfgtyfh bvnvhf cvnxjdjnf ssbf sjsfgyugthb jdbhbb vbg v jhby dhb sjhbb shfb </h1>
+            </div>
+        <div className="fade_bottom"></div>
+        </div>
+    )
+}
+
+export default Banner
